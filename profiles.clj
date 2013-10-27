a9c37d17a7af3f5eb1ca52b4ea20f72d71b5c481
Serafin Sedano <serafin.sedano@gmail.com>
2013-10-27 20:45:19 +0100

    Updating submodules

---
+++ profiles.clj
@@ -0,0 +1,10 @@
+{
+ :user {
+        :plugins [[lein-pprint "1.1.1"]
+                  [lein-clojars "0.9.0"]
+                  [lein-kibit "0.0.8"]
+                  [lein-pedantic "0.0.5"]
+                  [lein-ancient "0.4.4"]]
+        :dependencies [[clj-stacktrace "0.2.4"]]
+        }
+ }
